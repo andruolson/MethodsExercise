786ee0b47257093bfefac292755702d4c4e967fe
Andrew Olson <andruolson@gmail.com>
2025-08-27 18:07:44 -0600

    Cleaned up the code a little bit before starting second exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -8,36 +8,26 @@
             $"he saw {animal}'s all over the place! As he was walking he heard his favorite band " +
             $"{band} playing in the distance... Today was a good day!");
         }
-
+        
         static void Main(string[] args)
         {
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
-            
             Console.WriteLine("What is your favorite color?");
             string color = Console.ReadLine();
-            
             Console.WriteLine("Nice, that's my favorite color too!");
             Console.WriteLine("Two more questions and then i'll be all done!");
             Console.WriteLine("What is your favorite animal? Type your favorite animal out singular not plural");
             string animal = Console.ReadLine();
-            
             Console.WriteLine("Finally, what is your favorite band?");
             string band = Console.ReadLine();
-            
             Console.WriteLine("Thanks for all the info!");
             Console.WriteLine("Thinking...");
             Console.WriteLine("Thinking...");
             Console.WriteLine("Thinking...");
             Console.WriteLine("Thinking...");
             Console.WriteLine("........");
-
             WriteTheStory(name, color, animal, band); 
-            //Console.WriteLine($"{name} was walking down the road... The sky light up {color}... Everywhere he looked " +
-            //  $"he saw {animal}'s all over the place! As he was walking he heard his favorite band " +
-            //  $"{band} playing in the distance... Today was a good day!");
-
-
         }
     }
 }
